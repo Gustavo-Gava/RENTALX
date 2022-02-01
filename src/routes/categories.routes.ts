7c3d3528bd44e01ca3611130626060bf78843587
Gustavo Gava <gustavogava123@hotmail.com>
2022-02-01 08:30:57 -0300

    feat: added categories.routes && CreateCategoryService

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,24 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepositoty';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = []
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body
 
-  const category = new Category()
+  const createCategoryService =  new CreateCategoryService(categoriesRepository)
+  
+  createCategoryService.execute({ name, description })
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
+  return response.status(201).send()
+})
 
-  categories.push(category)
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list()
 
-  return response.status(201).json({ category })
+  return response.json(all)
 })
 
 export { categoriesRoutes };
